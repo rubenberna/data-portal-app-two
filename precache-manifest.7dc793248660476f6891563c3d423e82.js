b4f3f850ac030189e2c3e5a692387b84fee5819c
Ruben <ruben@pointbreak.co>
2020-12-28 10:27:57 +0100

    Updates

---
+++ precache-manifest.7dc793248660476f6891563c3d423e82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce384b8454735799e4ca1fb02db4891a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ea7a938a06f49cf6fe6",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "0372a0c5ea13ca25c6af",
+    "url": "./static/js/2.32c6cf5d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.32c6cf5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ea7a938a06f49cf6fe6",
+    "url": "./static/js/main.b332d760.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.b332d760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fad96e8416af95b702e7",
+    "url": "./static/js/runtime-main.3d2b268d.js"
+  }
+]);
