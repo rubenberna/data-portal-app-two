99f563770eca9a392b628b005784fbce69b291e2
Ruben <ruben@pointbreak.co>
2020-12-28 10:25:11 +0100

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from '@data-portal/app-two'
-import '@data-portal/app-two/dist/index.css'
+import { Component, metadata } from '@rubenberna/counter'
+import '@rubenberna/counter/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <Component/>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import {
+  button,
+  buttonsContainer,
+  container,
+  addButton,
+  subtractButton
+} from './_styles'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const Component = ({ state }) => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div style={container}>
+      { state?.username && <h4>Hello {state.username}!</h4>}
+      <h3>Current count: {count}</h3>
+      <div style={buttonsContainer}>
+        <button style={addButton} onClick={() => setCount(count + 1)}>
+          Add
+        </button>
+        <button style={subtractButton} onClick={() => setCount(count - 1)}>
+          Subtract
+        </button>
+      </div>
+    </div>
+  )
 }
+
+const metadata = {
+  title: 'Counter',
+  requiredPermissions: ['Guest'],
+  description: 'Simple counter',
+  icon: ''
+}
+
+Component.propTypes = {
+  state: PropTypes.shape({
+    isAuthenticated: PropTypes.bool,
+    token: PropTypes.string,
+    username: PropTypes.string,
+    role: PropTypes.string,
+    errorMessage: PropTypes.string
+  })
+}
+
+export  { Component, metadata }
